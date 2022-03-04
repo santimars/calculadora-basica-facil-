d7d19c149ca0371586ea739112bc8e3a3b0d258b
santimars <98999805+santimars@users.noreply.github.com>
2022-03-04 15:43:40 -0500

    Add files via upload

---
+++ tarea.py
@@ -0,0 +1,23 @@
+num1= float(input("Digite primer valor_"))
+num2= float(input("Digite segunda valor_"))
+print("selecioa la opcion que desees realizar...")
+print("1. Suma")
+print("2, Resta")
+print("3. Multiplicacion")
+print("4. Division")
+resultado= float(input("que operacion deseas hacer_?"))
+if resultado==1:
+    n=num1+num2
+    print(f"la respuesta es {n}")
+elif resultado==2:
+         n=num1-num2
+         print(f"la respuesta es {n}")
+            
+elif resultado==3:
+     n=num1*num2
+     print(f"La respuesta es {n}")
+     
+elif  resultado==4:
+    n=num1/num2
+     
+print("gracias por resolverlo")
